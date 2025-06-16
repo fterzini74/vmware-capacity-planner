5063c68fd806f8c9246123a3321fa46a54d29b6d
fterzini74 <88292850+fterzini74@users.noreply.github.com>
2025-06-16 09:41:50 +0200

    aggiunto componente FileUploadHandler mancante

---
+++ components/FileUploadHandler.jsx
@@ -0,0 +1,7 @@
+export default function FileUploadHandler({ onDataReady }) {
+  return (
+    <div className="p-4 border rounded bg-gray-100">
+      <p>📤 Carica il file RVTools (.xlsx) – componente attivo</p>
+    </div>
+  );
+}
